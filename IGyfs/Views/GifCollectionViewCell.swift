8de18d5795c63286955f335e7531b5729240e2cf
RaffDevs <rveronezdeveloper@gmail.com>
2023-02-16 21:50:22 -0300

    first commit

---
+++ IGyfs/Views/GifCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GiffCollectionViewCell.swift
+//  IGyfs
+//
+//  Created by Rafael Veronez Dias on 16/02/23.
+//
+
+import UIKit
+
+class GifCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var GifCell: UILabel!
+    
+}
